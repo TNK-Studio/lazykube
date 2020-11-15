d949b342d885c39bf75e09b2c3c213c2aeab31e0
elfgzp <me@elfgzp.cn>
2020-11-16 00:23:17 +0800

    docs: update scripts/install_update_linux.sh

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 # allow specifying different destination directory
-DIR="${DIR:-"/usr/local/bin"}"
+DIR="${DIR:-"/usr/bin/"}"
 
 # prepare the download URL
 GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/TNK-Studio/lazykube/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
-GITHUB_FILE="lazykube_linux_amd64.tar.gz"
-GITHUB_URL="https://github.com/TNK-Studio/lazykube/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}"
+echo "GITHUB_LATEST_VERSION ${GITHUB_LATEST_VERSION}"
+GITHUB_FILE="lazykube_linux_amd64"
+GITHUB_URL="https://github.com/TNK-Studio/lazykube/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}.tar.gz"
+echo "GITHUB_URL ${GITHUB_URL}"
 
 # install/update the local binary
 curl -L -o lazykube.tar.gz $GITHUB_URL --progress-bar
-tar -xzvf lazykube.tar.gz lazykube
-sudo mv -f lazykube "$DIR"
-rm lazykube.tar.gz
+tar -xzvf lazykube.tar.gz ${GITHUB_FILE}/lazykube
+sudo mv -f ${GITHUB_FILE}/lazykube "$DIR"
+echo "lazykube install to '${DIR}'"
+rm -rf lazykube.tar.gz ${GITHUB_FILE}
