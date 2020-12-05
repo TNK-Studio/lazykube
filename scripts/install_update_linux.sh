ec6626dbe2abb90ecf5b8b497949e88380463bf0
Elf <me@elfgzp.cn>
2020-12-05 20:39:35 +0800

    Update install_update_linux.sh

--- scripts/install_update_linux.sh
+++ scripts/install_update_linux.sh
@@ -2,6 +2,7 @@
 
 # allow specifying different destination directory
 DIR="${DIR:-"/usr/local/bin"}"
+PROXY="https://cool-moon-43e4.elfgzp.workers.dev/"
 
 ARCH=$(uname -m)
 case $ARCH in
@@ -13,14 +14,14 @@ case $ARCH in
 esac
 
 # prepare the download URL
-GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' https://github.com/TNK-Studio/lazykube/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
+GITHUB_LATEST_VERSION=$(curl -L -s -H 'Accept: application/json' ${PROXY}https://github.com/TNK-Studio/lazykube/releases/latest | sed -e 's/.*"tag_name":"\([^"]*\)".*/\1/')
 echo "GITHUB_LATEST_VERSION ${GITHUB_LATEST_VERSION}"
 GITHUB_FILE="lazykube_linux_${ARCH}"
 GITHUB_URL="https://github.com/TNK-Studio/lazykube/releases/download/${GITHUB_LATEST_VERSION}/${GITHUB_FILE}.tar.gz"
 echo "GITHUB_URL ${GITHUB_URL}"
 
 # install/update the local binary
-curl -L -o lazykube.tar.gz $GITHUB_URL --progress-bar
+curl -L -o lazykube.tar.gz $PROXY$GITHUB_URL --progress-bar
 tar -xzvf lazykube.tar.gz ${GITHUB_FILE}/lazykube
 sudo mv -f ${GITHUB_FILE}/lazykube "$DIR"
 echo "lazykube install to '${DIR}'"
